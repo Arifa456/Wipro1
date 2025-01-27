ba219b57489b713d87601114bc50927a79d06dd1
Arifa <arifabegum7076@gmail.com>
2025-01-27 14:21:00 +0530

    Hi

--- Hello.java
+++ Hello.java
@@ -1,16 +1,16 @@
 package com.ASS3;
 
-public class Hello {
+public class Hello  {
 
 	//instance  variable  object creation is needed for 
 	
 	int i =10 ; // instance variabe  // instance  Heap memory   objects
-	static int  k =18; //static  //class area  method area   u need no to create any object 
+	static int  k =18//static  //class area  method area   u need no to create any object 
 	public static void main(String[] args) {
 		Hello hello = new Hello();
    System.out.println(hello.i);
       int j=15; //local variable  //stack
-      System.out.println(Hello.k);  // static  String organisation = "wipro"
+      System.out.println(Hello World.k);  // static  String organisation = "wipro"
       
       System.out.println(j);
 System.out.println("first changes in git ");
